024d98387e2310bb7e0c3232e6bce730f82c8b07
ismail-dev-code <ismail.online0385678@gmail.com>
2024-11-30 07:04:07 +0600

    another example

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-const products = ['mobile', 'laptop', 'hp', 'lenevo'];
+// const products = ['mobile', 'laptop', 'hp', 'lenevo'];
 
-console.log(products.includes('lenevo'));
+// console.log(products.includes('lenevo'));
 
-if(products.includes('dell')){
-    console.log('party');
+// if(products.includes('dell')){
+//     console.log('party');
+// }
+// else{
+//     console.log('no cant eat we are fasting');
+// }
+
+// const FLProducts = ['blog', 'fb', 'twitter', 'website'];
+// console.log(FLProducts.includes('fb'));
+// if(FLProducts.includes('ig')){
+//     console.log('Faisal CEO');
+// }
+// else{ 
+//     console.log('Faisal Manager');
+// }
+
+const uae = ['dubai', 'sharjah', 'ajman', 'ras al khaima', 'fujairah'];
+console.log(uae.includes('sharjah'));
+
+if(uae.includes('fujairah')){
+    console.log('united arab emirates');
 }
 else{
-    console.log('no cant eat we are fasting');
-}
-
-const FLProducts = ['blog', 'fb', 'twitter', 'website'];
-console.log(FLProducts.includes('fb'));
-if(FLProducts.includes('ig')){
-    console.log('Faisal CEO');
-}
-else{ 
-    console.log('Faisal Manager');
+    console.log('Qatar');
 }
