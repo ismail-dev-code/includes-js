d6804c0de3e49531eafb0d7a00e049d8c83f646c
ismail-dev-code <ismail.online0385678@gmail.com>
2024-11-30 06:55:15 +0600

    includes

---
+++ index.js
@@ -0,0 +1,19 @@
+const products = ['mobile', 'laptop', 'hp', 'lenevo'];
+
+console.log(products.includes('lenevo'));
+
+if(products.includes('dell')){
+    console.log('party');
+}
+else{
+    console.log('no cant eat we are fasting');
+}
+
+const FLProducts = ['blog', 'fb', 'twitter', 'website'];
+console.log(FLProducts.includes('fb'));
+if(FLProducts.includes('ig')){
+    console.log('Faisal CEO');
+}
+else{ 
+    console.log('Faisal Manager');
+}
